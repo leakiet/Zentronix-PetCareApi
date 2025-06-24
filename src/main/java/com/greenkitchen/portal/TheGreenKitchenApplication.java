adb7e0c85e9ae6f372998d10e50dd27f8b84af36
Kiet Le <leakiet93@gmail.com>
2025-06-24 14:22:54 +0700

    first commit

---
+++ src/main/java/com/greenkitchen/portal/TheGreenKitchenApplication.java
@@ -0,0 +1,13 @@
+package com.greenkitchen.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGreenKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGreenKitchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenkitchen/portal/TheGreenKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenkitchen.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGreenKitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
